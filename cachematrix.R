d2e4377fcd9e15b5ef65480bf1dcca7a0434d6f7
Alex-Islander <alexvostroknutov@gmail.com>
2021-06-06 00:46:19 +1100

    Changed functions to calculate mean of a vector to functions s to calculate inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function creates list of functions to access and mutate input data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  result <- NULL #assigns "result" variable within makeCacheMatrix environment to null
+  set <- function(y=matrix()) {
+    x <<- y
+    result <<- NULL
+  } #function assigns values to objects "x" and "result" in parent environment 
+  get <- function() x #returns "x" value
+  setinverse <- function(inverse) result <<- inverse #assigns value to "result" object in parent environment
+  getinverse <- function() result #returns ""result" value
+  list(set = set, get = get, 
+       setinverse = setinverse,
+       getinverse = getinverse) #creates list of functions
 }
 
 
-## Write a short comment describing this function
+## Function checks if no result is stored in cache and calculates inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  result <- x$getinverse()
+  if(!is.null(result)) {
+    message("getting cached data")
+    return(result)
+  }
+  data <- x$get()
+  result <- solve(data, ...)
+  x$setinverse(result)
+  result
 }
